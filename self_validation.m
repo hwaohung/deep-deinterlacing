fb81ca353aae7e14d57ec64d46dae8bd7b2ef602
Johnny <johny60207@gmail.com>
2016-11-22 01:48:06 +0800

    Self validation refine

--- self_validation.m
+++ self_validation.m
@@ -4,13 +4,13 @@ function [deinterlaced_frames] = self_validation(frames)
 
     [hei, wid, cnt] = size(frames);
     
-    methods = 6;
+    methods = 5;
     
     field_map = uint8(zeros(hei/2, wid, cnt, methods));
     diff_map = zeros(hei/2 + 1, wid, cnt, methods);
     for method = 1:methods
-        d1_frames = deinterlaces(frames, 1, method);
-        d2_frames = deinterlaces(frames, 0, method);
+        d1_frames = deinterlace(frames, 1, method);
+        d2_frames = deinterlace(frames, 0, method);
         
         for i = 1:size(frames, 3)
             if mod(i, 2)
@@ -67,17 +67,181 @@ function [deinterlaced_frames] = self_validation(frames)
     end
 end
 
+function [images] = deinterlace(fields, is_odd, method)
+    if method == 1
+        images = method1(fields, is_odd);
+    elseif method == 2
+        images = method2(fields, is_odd);
+    elseif method == 3
+        images = method3(fields, is_odd);
+    elseif method == 4
+        images = method4(fields, is_odd);
+    elseif method == 5
+        images = method5(fields, is_odd);
+    elseif method == 6
+        images = DeepTemp(fields, 100000, is_odd);
+    end
+end
+
+function [images] = method1(fields, is_odd)
+    images = zeros(size(fields), class(fields));
+    
+    fields = im2double(fields);
+    mask = [0.5, 0, 0; 0, 0, 0.5];
+    for i = 1:size(fields, 3)
+        field = fields(:, :, i);
+        image = field;
+                
+        if mod(i, 2) == is_odd
+            field = field(1:2:end, :);
+            field = padarray(field, [0, 1], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(2:2:end-1, :) = interpolation;
+            image(end, :) = image(end-1, :);
+        else                        
+            field = field(2:2:end, :);
+            field = padarray(field, [0, 1], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(3:2:end-1, :) = interpolation;
+            image(1, :) = image(2, :);
+        end
+        
+        images(:, :, i) = im2uint8(image);
+    end
+end
+
+function [images] = method2(fields, is_odd)
+    images = zeros(size(fields), class(fields));
+    
+    fields = im2double(fields);
+    mask = [0.5; 0.5];
+    for i = 1:size(fields, 3)
+        field = fields(:, :, i);
+        image = field;
+                
+        if mod(i, 2) == is_odd
+            field = field(1:2:end, :);
+            interpolation = conv2(field, mask, 'valid');
+            image(2:2:end-1, :) = interpolation;
+            image(end, :) = image(end-1, :);
+        else                        
+            field = field(2:2:end, :);
+            interpolation = conv2(field, mask, 'valid');
+            image(3:2:end-1, :) = interpolation;
+            image(1, :) = image(2, :);
+        end
+        
+        images(:, :, i) = im2uint8(image);
+    end
+end
+
+function [images] = method3(fields, is_odd)
+    images = zeros(size(fields), class(fields));
+    
+    fields = im2double(fields);
+    mask = [0, 0, 0.5; 0.5, 0, 0];
+    for i = 1:size(fields, 3)
+        field = fields(:, :, i);
+        image = field;
+                
+        if mod(i, 2) == is_odd
+            field = field(1:2:end, :);
+            field = padarray(field, [0, 1], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(2:2:end-1, :) = interpolation;
+            image(end, :) = image(end-1, :);
+        else                        
+            field = field(2:2:end, :);
+            field = padarray(field, [0, 1], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(3:2:end-1, :) = interpolation;
+            image(1, :) = image(2, :);
+        end
+        
+        images(:, :, i) = im2uint8(image);
+    end
+end
+
+function [images] = method4(fields, is_odd)
+    images = zeros(size(fields), class(fields));
+    
+    fields = im2double(fields);
+    window = 3;
+    padding = (window-1) / 2;
+    mask = [0.0533, 0.3935, 0.0533;
+            0.0533, 0.3935, 0.0533];
+    
+    for i = 1:size(fields, 3)
+        field = fields(:, :, i);
+        image = field;
+                
+        if mod(i, 2) == is_odd
+            field = field(1:2:end, :);
+            field = padarray(field, [1, padding], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(2:2:end, :) = interpolation(2:end, :);
+        else                        
+            field = field(2:2:end, :);
+            field = padarray(field, [1, padding], 'symmetric');
+            interpolation = conv2(field, mask, 'valid');
+            image(1:2:end, :) = interpolation(1:end-1, :);
+        end
+        
+        images(:, :, i) = im2uint8(image);
+    end
+end
+
+function [images] = method5(fields, is_odd)
+    images = zeros(size(fields), class(fields));
+    
+    fields = im2double(fields);
+    for i = 1:size(fields, 3)
+        field = fields(:, :, i);
+        
+        % Bilinear interpolation
+        if mod(i, 2) == is_odd
+            image = field;
+            tmp = field(1:2:end, :);
+            tmp = imresize(tmp, size(field), 'bilinear');
+            image(2:2:end, :) = tmp(2:2:end, :);
+        else            
+            image = field;
+            tmp = field(2:2:end, :);
+            tmp = imresize(tmp, size(field), 'bilinear');
+            image(1:2:end, :) = tmp(1:2:end, :);
+        end        
+        
+        %{
+        hdint = vision.Deinterlacer('Method', 'Vertical temporal median filtering', 'TransposedInput', false);
+            
+        if mod(i, 2) == is_odd
+            image = step(hdint, field);
+        else
+            field(1:end-1, :) = field(2:end, :);
+            image = step(hdint, field);
+            image(2:end, :) = image(1:end-1, :);
+        end
+        %}
+        
+        images(:, :, i) = im2uint8(image);
+    end
+end
+
+%{
 function [images] = deinterlaces(fields, is_odd, method)
     if method < 6
         images = zeros(size(fields), 'uint8');
         for i = 1:size(fields, 3)
             images(:, :, i) = deinterlace(fields(:, :, i), mod(i, 2) == is_odd, method);
         end
+        
     else
         images = DeepTemp(fields, 100000, is_odd);
     end
 end
+%}
 
+%{
 function [image] = deinterlace(field, odd, method)
     if method == 1
         field = im2double(field);
@@ -138,19 +302,6 @@ function [image] = deinterlace(field, odd, method)
         
         image = im2uint8(image);
     elseif method == 4
-        % Bicubic interpolation
-        if ~odd
-            image = field;
-            tmp = field(2:2:end, :);
-            tmp = imresize(tmp, size(field), 'bilinear');
-            image(1:2:end, :) = tmp(1:2:end, :);
-        else
-            image = field;
-            tmp = field(1:2:end, :);
-            tmp = imresize(tmp, size(field), 'bilinear');
-            image(2:2:end, :) = tmp(2:2:end, :);
-        end
-    elseif method == 5
         % Gaussian method
         field = im2double(field);
         image = field;
@@ -172,7 +323,7 @@ function [image] = deinterlace(field, odd, method)
         end
         
         image = im2uint8(image);
-        
+    elseif method == 5
         %{
         hdint = vision.Deinterlacer('Method', 'Vertical temporal median filtering', 'TransposedInput', false);
             
@@ -184,8 +335,24 @@ function [image] = deinterlace(field, odd, method)
             image = step(hdint, field); 
         end
         %}
+        
+        %{
+        % Bilinear interpolation
+        if ~odd
+            image = field;
+            tmp = field(2:2:end, :);
+            tmp = imresize(tmp, size(field), 'bilinear');
+            image(1:2:end, :) = tmp(1:2:end, :);
+        else
+            image = field;
+            tmp = field(1:2:end, :);
+            tmp = imresize(tmp, size(field), 'bilinear');
+            image(2:2:end, :) = tmp(2:2:end, :);
+        end
+        %}
     end
 end
+%}
 
 function [map] = calc_diff_map(frame1, frame2)
     map = abs(frame1 - frame2);
